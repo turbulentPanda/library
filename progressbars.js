9a8f20d38aeb194cdde52e8c07d5b211b207a012
Christopher Marchant <cmarch240@gmail.com>
2021-11-03 16:33:15 -0500

    Downloaded progressbar.js and created circular progressbar for book count

---
+++ progressbars.js
@@ -0,0 +1,14 @@
+// Book Count Progress Bar
+const percentage = 4 / 7;
+const bookCountProgressBar = new ProgressBar.Circle('.book-count-progressbar', {
+    color: '#623b5a',
+    strokeWidth: 4,
+    trailColor: 'white',
+    text: {
+        value: `You have read <span class="myspan">${(percentage * 100).toFixed(0)}%</span><br>of your bookshelf`,
+        className: 'bookCountProgressBarText',
+    },
+});
+bookCountProgressBar.animate(percentage, {
+    duration: 2000,
+});
